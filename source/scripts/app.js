81367cb6dce682d0e6f3d2d03869d7b934080e47
Michael Lajlev <lajlev@gmail.com>
2013-09-18 11:23:19 +0200

    removed foundation

--- source/scripts/app.js
+++ source/scripts/app.js
@@ -1,6 +1,31 @@
 //=require vendor/jquery
+//=require vendor/jquery.jribbble
 //=require vendor/jquery.event.swipe
 //=require vendor/unslider.min
+//=require vendor/jquery.instagram
+
+$(document).ready(function() {
+
+  // API Ref: http://api.dribbble/players/:id/shots
+  $.jribbble.getShotsByPlayerId('victorerixon', function (playerShots) {
+    var html = [];
+    $.each(playerShots.shots, function (i, shot) {
+      html.push('<a href="' + shot.url + '" title="' + shot.title + '">');
+      html.push('<img class="rounded" src="' + shot.image_url  + '" ');
+      html.push('alt="' + shot.title + '"></a></li>');
+    });
+
+    $('#dribble-shots').html(html.join(''));
+  }, {page: 1, per_page: 12});
+
+
+  $("#instagram-image").instagram({
+    userId: '1853198',
+    show: 1,
+    accessToken: '1853198.ab103e5.af22f08d75fd47d781a516698a2c2104',
+    image_size: 'low_resolution'
+  });
+});
 
 
 //// Slider settings ////
